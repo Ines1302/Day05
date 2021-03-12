d314912c079c1f16860c4a9c11a2001f678e1d34
Ines1302 <seeking.knowledge13@gmail.com>
2021-03-12 22:49:30 +0100

    Strings and Variables part 2

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  var myAdjective = "the best!";
+
+  var myString = "Mistral is ";
+
+  myString += myAdjective;
+
+  return myString;
+}
+console.log(main());
+module.exports = main;
