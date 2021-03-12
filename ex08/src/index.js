a09444801299a9265daab44f2498fde8ed55a236
Ines1302 <seeking.knowledge13@gmail.com>
2021-03-12 22:53:16 +0100

    Length of a String

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  var lastName = "Doe";
+
+  var lastNameLenght;
+
+  lastNameLenght = lastName.length;
+
+  return lastNameLenght;
+}
+console.log(main());
+module.exports = main;
